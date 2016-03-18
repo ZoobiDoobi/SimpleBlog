a337b85cd6228e6699e9942ede5a373555497c06
ZoobiDoobi <bscsf12m033@gmail.com>
2016-03-18 09:34:05 +0500

    Addes Styles and Scripts and did bundling and minification

--- SimpleBlog/Global.asax.cs
+++ SimpleBlog/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Web.Optimization;
+using SimpleBlog.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +15,7 @@ namespace SimpleBlog
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(System.Web.Optimization.BundleTable.Bundles);
         }
     }
 }
